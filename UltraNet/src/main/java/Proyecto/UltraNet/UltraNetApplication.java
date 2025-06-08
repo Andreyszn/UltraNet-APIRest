732397a1384e5cb069af28d297100fa63562dd8f
Davidnugu <nugudavid@gmail.com>
2025-06-07 20:12:36 -0600

    Creación del proyecto

---
+++ UltraNet/src/main/java/Proyecto/UltraNet/UltraNetApplication.java
@@ -0,0 +1,13 @@
+package Proyecto.UltraNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltraNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltraNetApplication.class, args);
+	}
+
+}
---
+++ UltraNet/src/test/java/Proyecto/UltraNet/UltraNetApplicationTests.java
@@ -0,0 +1,13 @@
+package Proyecto.UltraNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltraNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
