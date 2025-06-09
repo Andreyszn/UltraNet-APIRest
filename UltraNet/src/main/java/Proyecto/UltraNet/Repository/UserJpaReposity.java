a0f236d812b12542794a6e7692f89ceb30ee4afa
Andreyszn <AndreySotoVargas999@gmail.com>
2025-06-08 19:16:51 -0600

    User parte 1

--- UltraNet/src/main/java/Proyecto/UltraNet/Repository/UserJpaReposity.java
+++ UltraNet/src/main/java/Proyecto/UltraNet/Repository/UserJpaReposity.java
@@ -2,7 +2,9 @@ package Proyecto.UltraNet.Repository;
 
 import Proyecto.UltraNet.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserJpaReposity extends JpaRepository <User, Integer> {
 
 }
--- UltraNet/src/main/java/Proyecto/UltraNet/Repository/UserRepository.java
+++ UltraNet/src/main/java/Proyecto/UltraNet/Repository/UserRepository.java
@@ -1,5 +1,26 @@
 package Proyecto.UltraNet.Repository;
 
-public class UserRepository {
+import Proyecto.UltraNet.Model.User;
 
+public class UserRepository extends CRUDMemory <User> {
+
+    public UserRepository() {
+        //aqui agregamos usuarios a lo baboso luego
+    }
+    
+    @Override
+    public User edit(User user) {
+        for(User element: data){
+            if(element.getId().intValue()==user.getId().intValue()) {
+                if(user.getName()!=null){
+                    element.setName(user.getName());
+                }
+                if (user.getType()!=null) {
+                    element.setType(user.getType());
+                }
+                return element;
+            }
+        }
+        return new User();
+    }
 }
