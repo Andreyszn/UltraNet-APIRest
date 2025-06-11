c1bb54b7f014d03c2c1c8bd568b1988e9acd1d17
Charli344 <guevaracharly4@gmail.com>
2025-06-11 13:23:59 -0600

    Arreglo de errores en HardwareService

--- UltraNet/src/main/java/Proyecto/UltraNet/Service/HardwareService.java
+++ UltraNet/src/main/java/Proyecto/UltraNet/Service/HardwareService.java
@@ -1,19 +1,11 @@
 package Proyecto.UltraNet.Service;
 
-<<<<<<< HEAD
-import UltraNet.Model.Hardware;
-import UltraNet.Model.Identifiable;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import UltraNet.Repository.HardwareRepository;
-=======
 import Proyecto.UltraNet.Model.Hardware;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import Proyecto.UltraNet.Repository.HardwareRepository;
 
 import java.util.ArrayList;
->>>>>>> Charlie
 
 
 @Service
@@ -22,13 +14,6 @@ public class HardwareService {
     HardwareRepository repository;
 
     public ArrayList<Hardware> getAll(){return repository.getAll();}
-<<<<<<< HEAD
-    public Hardware add(Hardware hardware){return repository.add(harware);}
-    public Hardware edit(Hardware hardware){return repository.edit(hardware);}
-    public Hardware update(Hardware hardware){return repository.update(hardware);}
-    public Hardware delete(Identifiable id){return repository.delete(id.getId());}
-    public Hardware search(Identifiable id){return repository.search(id.getId());}
-=======
 
     public Hardware add(Hardware hardware){return repository.add(hardware);}
 
@@ -39,6 +24,5 @@ public class HardwareService {
     public Hardware editHardware(Hardware hardware){return repository.edit(hardware);}
 
     public Hardware updateHardware(Hardware hardware){return repository.update(hardware);}
->>>>>>> Charlie
 
 }
