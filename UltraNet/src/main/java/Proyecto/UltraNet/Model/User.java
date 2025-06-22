a6de0d063dd4e6fc91f67ec5079b9c3de8a6e407
Andreyszn <AndreySotoVargas999@gmail.com>
2025-06-21 18:46:13 -0600

    Fixing ?

--- UltraNet/src/main/java/Proyecto/UltraNet/Model/User.java
+++ UltraNet/src/main/java/Proyecto/UltraNet/Model/User.java
@@ -1,12 +1,12 @@
 package Proyecto.UltraNet.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
+
+    // jajajajajajajaja
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
