6395ed9b3427aee83713d255f42f4925f9db9e61
Luis-Cascante <luisalonsocascante@gmail.com>
2025-06-26 20:31:12 -0600

    modificaciones de StoreService

--- UltraNet/src/main/java/Proyecto/UltraNet/Service/StoreService.java
+++ UltraNet/src/main/java/Proyecto/UltraNet/Service/StoreService.java
@@ -8,14 +8,34 @@ import Proyecto.UltraNet.Repository.StoreJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class StoreService {
 
-    @Autowired
-    private StoreJpaRepository storeJpaRepository;
+    private StoreJpaRepository storeRepository;
+    private UserService userService;
+    private HardwareService hardwareService;
 
-    @Autowired
-    private HardwareRepositoryJpa hardwareRepositoryJpa;
+    public StoreService(StoreJpaRepository storeRepository, UserService userService, HardwareService hardwareService) {
+        this.storeRepository = storeRepository;
+        this.userService = userService;
+        this.hardwareService = hardwareService;
+    }
+
+    public List<Store> getAll(){
+        return storeRepository.findAll();
+    }
+
+    public Store add(Store storeDTO){
+        User user = userService.findUserById(storeDTO);
+        Hardware hardware = hardwareService.findHardwareById(storeDTO);
+        Store store = new Store();
+        store.setUser(user);
+        store.setHardware(hardware);
+        store.setQuantity(storeDTO.getQuantity());
+        return storeRepository.save(store);
+    }
 
     //Add item to cart
     //Remove from cart
